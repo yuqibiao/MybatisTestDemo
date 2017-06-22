4068f5dbf81348de8e8eb99082414d6d205db224
yyyu <13165691974@163.com>
2017-06-22 22:38:28 +0800

    sql文件

---
+++ resource/doc/test.sql
@@ -0,0 +1,17 @@
+use yu;
+
+CREATE TABLE user(
+  id INT NOT NULL  AUTO_INCREMENT,
+  username VARCHAR(10) NOT NULL ,
+  sex CHAR(2) DEFAULT '0',
+  birthday DATE,
+  address VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
+INSERT  INTO user(username, sex, birthday, address) VALUES ('张三','1','1992-06-05','湖北武汉');
+INSERT  INTO user(username, sex, birthday, address) VALUES ('李四','1','1992-06-05','湖北武汉');
+INSERT  INTO user(username, sex, birthday, address) VALUES ('王五','1','1992-06-05','湖北武汉');
+INSERT  INTO user(username, sex, birthday, address) VALUES ('张小华','1','1992-06-05','广东深圳');
+
+SELECT * FROM user;
